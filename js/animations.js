c8952b5d73943e6eb7d7ad0fadb9ae9ebc7481c6
vladimir12213 <vladimir@diakonov.ru>
2022-12-30 17:01:03 +0300

    all parts

---
+++ js/animations.js
@@ -0,0 +1,46 @@
+const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+
+function increaseNumberAnimationStep(i, element, endNumber) {
+    if (i <= endNumber) {
+        if (i === endNumber) {
+            element.innerText = i + '+';
+        } else {
+            element.innerText = i;
+        }
+
+        i += 10;
+
+        setTimeout(function () {
+            increaseNumberAnimationStep(i, element, endNumber);
+        }, INCREASE_NUMBER_ANIMATION_SPEED);
+    }
+}
+
+function initIncreaseNumberAnimation() {
+    const element = document.querySelector('.features__clients-count');
+
+    increaseNumberAnimationStep(0, element, 5000);
+}
+
+initIncreaseNumberAnimation();
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form__group');
+        formContainer.classList.add('form__other-input');
+
+        const input = document.createElement('input');
+        input.placeholder = "Введите ваш вариант";
+        input.type = "text";
+
+        formContainer.appendChild(input);
+        document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    }
+
+    if (event.target.value !== 'other') {
+        const otherInput = document.querySelector('.form__other-input');
+        document.querySelector('#form form').removeChild(otherInput);
+    }
+});
+
+
